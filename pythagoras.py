0048675737c491bf4779f143d55ccd771e7d626d
Timo Eichenberger <timo.limmat@gmail.com>
2022-08-13 18:17:58 +0200

    Variable A and h added.

--- pythagoras.py
+++ pythagoras.py
@@ -31,6 +31,12 @@ def main():
         b = calculate_b(a, c)
         print(b)
 
+    A = calculate_A(a, b)
+    print(f'Fläche: {A}')
+
+    h = calculate_h(A, c)
+    print(f'Höhe: {h}')
+
 
 def calculate_c(a, b):
     return sqrt(a*a + b*b)
@@ -42,6 +48,14 @@ def calculate_a(b, c):
 
 def calculate_b(a, c):
     return sqrt(c*c - a*a)
+
+
+def calculate_A(a, b):
+    return (a*b/2)
+
+
+def calculate_h(A, c):
+    return (A*2/c)
 
 
 if __name__ == '__main__':
